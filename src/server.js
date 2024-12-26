2a8416cd76315027b2e0be94448c7d75e535d287
liahychka <157300297+liahychka@users.noreply.github.com>
2024-12-26 19:45:59 +0100

    .

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import * as fs from 'node:fs';
 import swaggerUI from 'swagger-ui-express';
-import path from "node:path";
+import path from 'node:path';
 import cors from 'cors';
 import express from 'express';
 import 'dotenv/config';
@@ -8,9 +8,9 @@ import pino from 'pino-http';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import routes from './routers/contacts.js';
-import cookieParser from "cookie-parser";
-import authRouter from "./routers/auth.js";
-import { authenticate } from "./middlewares/authenticate.js";
+import cookieParser from 'cookie-parser';
+import authRouter from './routers/auth.js';
+import { authenticate } from './middlewares/authenticate.js';
 
 const swaggerDocument = JSON.parse(
   fs.readFileSync(path.resolve('docs/swagger.json'), 'utf-8'),
@@ -20,7 +20,7 @@ const app = express();
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.use("/photo", express.static(path.resolve("src/public/photo")));
+app.use('/photo', express.static(path.resolve('src/public/photo')));
 
 app.use(cors());
 
@@ -31,31 +31,30 @@ app.use('/auth', authRouter);
 app.use(authenticate, routes);
 
 app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World!',
-    });
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
   });
+});
 
 app.use('*', notFoundHandler);
 
 app.use(errorHandler);
 
 export function setupServer() {
-        try {
-            const PORT = process.env.PORT || 3000;
-            app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
-
